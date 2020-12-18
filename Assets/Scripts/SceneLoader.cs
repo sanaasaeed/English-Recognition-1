64a200e3862e860d75646290d1d802376ec9b03a
Sana Saeed <sanaasaeed@users.noreply.github.com>
2020-12-18 17:49:28 +0500

    Scene loader done

---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    void LoadNextScene() {
+        int currentBuildIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentBuildIndex + 1);
+    }
+    
+}
