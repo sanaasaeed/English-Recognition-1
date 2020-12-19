ec036a21e1f2a112bfbdbc7a1611558b3562c9a3
Sana Saeed <sanaasaeed@users.noreply.github.com>
2020-12-19 15:28:42 +0500

    Loading Screen added

--- Assets/Scripts/SceneLoader.cs
+++ Assets/Scripts/SceneLoader.cs
@@ -1,11 +1,38 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
-public class SceneLoader : MonoBehaviour
-{
+public class SceneLoader : MonoBehaviour {
+    public GameObject loadingScreen;
+
+    public Slider slider;
+    // public void LoadNextScene() {
+    //     int currentBuildIndex = SceneManager.GetActiveScene().buildIndex;
+    //     SceneManager.LoadScene(currentBuildIndex + 1);
+    // }
+    public static void LoadNextSceneWithoutLoading() {
+        
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        AsyncOperation operation =  SceneManager.LoadSceneAsync(currentSceneIndex + 1);
+    }
+
+    public void LoadFirstScene() {
+        SceneManager.LoadScene(0);
+    }
     public void LoadNextScene() {
-        int currentBuildIndex = SceneManager.GetActiveScene().buildIndex;
-        SceneManager.LoadScene(currentBuildIndex + 1);
+        StartCoroutine(LoadSceneAsync());
+    }
+
+    IEnumerator LoadSceneAsync() {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        AsyncOperation operation =  SceneManager.LoadSceneAsync(currentSceneIndex + 1);
+        loadingScreen.SetActive(true);
+        while (!operation.isDone) {
+            float progress = Mathf.Clamp01(operation.progress / 0.9f);
+            slider.value = progress;
+            yield return null;
+        }
     }
 
     public void OnQuit() {
