27680505a8d5760597e528cfbd29521e434bc0ac
Sana Saeed <sanaasaeed@users.noreply.github.com>
2020-12-19 18:12:30 +0500

    Popup menu done

---
+++ Assets/Scripts/PopupMenu.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PopupMenu : MonoBehaviour {
+    public static bool IsPopUpOpened = true;
+    public GameObject popupMenu;
+
+    private void OnEnable() {
+        Time.timeScale = 0f;
+    }
+
+    public void Update() {
+        
+        if (Input.GetKeyDown(KeyCode.I)) {
+            if (!IsPopUpOpened) {
+                OpenPopUp();
+            } 
+        }
+        else if (Input.GetKeyDown(KeyCode.Escape)) {
+            if (IsPopUpOpened) {
+                ClosePopUp();
+            }
+        }
+    }
+
+    public void OpenPopUp() {
+        popupMenu.SetActive(true);
+        Time.timeScale = 0f;
+        IsPopUpOpened = true;
+    }
+
+    public void ClosePopUp() {
+        popupMenu.SetActive(false);
+        Time.timeScale = 1f;
+        IsPopUpOpened = false;
+    }
+}
